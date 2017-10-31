fc2c539f687297451bf4d2faa491a40885d94da6
TrueFalseTF <dimon_mcensk@mail.ru>
2017-10-31 15:02:44 +0300

    Добавлен пустой конструктор

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -11,7 +11,12 @@ namespace Snake
         public int x;
         public int y;
         public char sym;
-       
+
+        public Point()
+        {
+            
+        }
+
         public Point(int _x, int _y, char _sym)
         {
             x = _x;
